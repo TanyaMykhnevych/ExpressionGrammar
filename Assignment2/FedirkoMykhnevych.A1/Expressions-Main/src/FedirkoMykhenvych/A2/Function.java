67a8eeea1190ebd8680cf622be8c4e2497454ce5
tatyana.mykhnevych <tatyana.mykhnevych@gmail.com>
2019-12-04 20:48:45 +0200

    Small change for functions

--- Assignment2/FedirkoMykhnevych.A1/Expressions-Main/src/FedirkoMykhenvych/A2/Function.java
+++ Assignment2/FedirkoMykhnevych.A1/Expressions-Main/src/FedirkoMykhenvych/A2/Function.java
@@ -1,29 +1,35 @@
 package FedirkoMykhenvych.A2;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Function implements Scope{
+	private String name;
+
+	public Function(String name) {
+		this.name = name;
+	}
+
+	public Function getEnclosingScope() {
+		return null;
+	}
+
+	public void define(Symbol sym) {		
+	}
+
+	public Symbol resolve(String name) {
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
 
 	@Override
 	public String getScopeName() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Scope getEnclosingScope() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void define(Symbol sym) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Symbol resolve(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 	
 }
--- Assignment2/FedirkoMykhnevych.A1/Expressions-Main/src/FedirkoMykhenvych/A2/FunctionListener.java
+++ Assignment2/FedirkoMykhnevych.A1/Expressions-Main/src/FedirkoMykhenvych/A2/FunctionListener.java
@@ -9,60 +9,59 @@ import FedirkoMykhnevych.A2.OfpBaseListener;
 import FedirkoMykhnevych.A2.OfpParser;
 import FedirkoMykhnevych.A2.OfpParser.FunctionDeclarationContext;
 
-public class FunctionListener extends OfpBaseListener{
-	
+public class FunctionListener extends OfpBaseListener {
+
 	OfpParser parser;
-	
+
 	final Map<String, Function> declaredFunctions;
-	
+
 	ParseTreeProperty<Scope> scopes;
-	
+
 	Scope currentScope = null;
-	
-	boolean isMain; // not sure if we need this
-	
+
+	boolean mainWasDeclared; // not sure if we need this
+
 	public FunctionListener(final Map<String, Function> functions, ParseTreeProperty<Scope> scopes, OfpParser parser) {
 		this.declaredFunctions = functions;
 		this.scopes = scopes;
-		this.parser = parser;	
+		this.parser = parser;
 	}
-	
-	@Override public void enterFunctionDeclaration(OfpParser.FunctionDeclarationContext ctx) {
-		String functionName = ctx.IDENTIFIER().getText();		
-		Function function = declaredFunctions.get(functionName);
+
+	@Override
+	public void enterFunctionDeclaration(OfpParser.FunctionDeclarationContext ctx) {
+		String functionName = ctx.IDENTIFIER().getText();
+		Function function = new Function(functionName);
 		currentScope = function;
-		
-		if(functionName == "main") {
-			String returnValue = ctx.returnValue().getText();
-			if(returnValue != "void")
-					ErrorPrinter
-						.printFullError(
-							parser, 
-							ctx.IDENTIFIER().getSymbol(), 
-							"error: main() function must be void", 
-							functionName, 
-							"location" + currentScope.getScopeName());
-		}
-		else {			
-			
-			if(declaredFunctions.containsKey(functionName))
-				ErrorPrinter
-					.printFullError(
-						parser, 
-						ctx.IDENTIFIER().getSymbol(), 
-						"function ", 
-						functionName, 
-						currentScope.getScopeName());
-		}
-		
-		//saveScope(ctx, function);
+
+		if (declaredFunctions.containsKey(functionName))
+			ErrorPrinter.printFullError(parser, ctx.IDENTIFIER().getSymbol(), "function ", functionName,
+					currentScope.getScopeName());
+
+		saveScope(ctx, function);
 	}
-	
-	@Override public void exitFunctionDeclaration(OfpParser.FunctionDeclarationContext ctx) {
+
+	@Override
+	public void exitFunctionDeclaration(OfpParser.FunctionDeclarationContext ctx) {
 		currentScope = currentScope.getEnclosingScope();
 	}
-	
-	private void saveScope(ParserRuleContext ctx, Scope s) {
+
+	@Override
+	public void enterMainFunctionDeclaration(OfpParser.MainFunctionDeclarationContext ctx) {
+		if (mainWasDeclared) {
+			ErrorPrinter.printFullError(parser, null, "function ", "main", currentScope.getScopeName());
+		}
+
+		mainWasDeclared = true;
+		// saveScope(ctx, function);
+	}
+
+	@Override
+	public void exitMainFunctionDeclaration(OfpParser.MainFunctionDeclarationContext ctx) {
+
+	}
+
+	private void saveScope(ParserRuleContext ctx, Function s) {
+		declaredFunctions.put(s.getScopeName(), s);
 		scopes.put(ctx, s);
 	}
 }
